86da000fe069f4add1d181dd3a3fb4cab4eac944
ThoreauBai <china_baizhuangli@163.com>
2016-12-22 09:11:48 +0800

    删除无用包

--- source/com/grademanager/student/handler/TestHandler.java
+++
@@ -1,9 +0,0 @@
-package com.grademanager.student.handler;
-
-import com.grademanager.beans.Student;
-import com.grademanager.handler.BaseHandler;
-
-public interface TestHandler extends BaseHandler<Student> {
-	
-	
-}
